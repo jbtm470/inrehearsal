297dfd606703228bfa96893b92dade269933aa16
jbtm470 <jbtm470@gmail.com>
2021-09-16 20:38:26 +0100

    New listscript

--- InRehearsal.py
+++ InRehearsal.py
@@ -34,6 +34,8 @@ class ListScriptIntentHandler(AbstractRequestHandler):
         return is_intent_name("ListScriptIntent")(handler_input)
 
     def handle(self, handler_input):
+        scriptname = handler_input.request_envelope.request.intent.slots['scriptname'].value
+
         try:
             data = ddb.scan(
                 TableName="Scripts"
@@ -43,10 +45,13 @@ class ListScriptIntentHandler(AbstractRequestHandler):
             raise(e)
 
         speech_text = ""
+        script_match = 0
         for i in data['Items']:
-
             st =i['ScriptTitle']['S']
             aut=i['Author']['S']
+
+            if st == scriptname:
+                script_match =1
 
             if speech_text == "":
                 pause = ""
@@ -55,10 +60,16 @@ class ListScriptIntentHandler(AbstractRequestHandler):
 
             speech_text = speech_text + pause + st + " written by "+ aut
 
-        speech_text = "I have the following scripts." + pause + speech_text
-        handler_input.response_builder.speak(speech_text).set_should_end_session(False)
-
-
+        if scriptname == "":
+            speech_text = "I have the following scripts." + pause + speech_text
+        else:
+            if script_match == 1:
+                speech_text = "I have the script " + scriptname +"."
+            else: 
+                speech_text = "I'm sorry, I do not have the script " + scriptname +"."
+            
+            handler_input.response_builder.speak(speech_text).set_should_end_session(False)
+    
         return handler_input.response_builder.response    
 
 class ReadScriptIntentHandler(AbstractRequestHandler):
@@ -102,7 +113,46 @@ class ReadScriptIntentHandler(AbstractRequestHandler):
    
         return handler_input.response_builder.response
 
+class RehearseScriptIntentHandler(AbstractRequestHandler):
 
+    def can_handle(self, handler_input):
+        return is_intent_name("RehearseScriptIntent")(handler_input)
+
+    def handle(self, handler_input):
+        scriptname = handler_input.request_envelope.request.intent.slots['scriptname'].value
+        print ("---"+ scriptname)
+        handler_input.response_builder.speak("Lets rehearse "+ scriptname).set_should_end_session(False)
+        numAct = '1'
+        numScene = '1'
+        try:
+            data = ddb.scan(
+                TableName="Macbeth",
+                ExpressionAttributeValues={
+                    ':act': {
+                        'N': "1"
+                    },
+                    ':scene': {
+                        'N': "1"
+                    },
+                    ':include': {
+                        'N': "1"
+                    }
+                },
+                FilterExpression="act = :act and scene = :scene and include = :include",
+                ProjectionExpression="act,scene,Line,direction"
+            )
+        except BaseException as e:
+            print(e)
+            raise(e)
+
+        print(data['Items'])
+        a=data['Items']
+
+        for i in range(len(a)) : 
+            for j in range(len(a[i])) : 
+                print(a[i][j], end=" ")
+   
+        return handler_input.response_builder.response
 
 
 sb = SkillBuilder()
@@ -110,6 +160,7 @@ sb.add_request_handler(LaunchRequestHandler())
 sb.add_exception_handler(CatchAllExceptionHandler())
 sb.add_request_handler(ListScriptIntentHandler())
 sb.add_request_handler(ReadScriptIntentHandler())
+sb.add_request_handler(RehearseScriptIntentHandler())
 
 def handler(event, context):
     return sb.lambda_handler()(event, context)
